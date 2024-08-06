9dede4e9c5d669ad11352bb30e1606244c81daf5
Ankit Dey <personal.ankitdey@gmail.com>
2024-08-06 23:51:14 +0530

    added background photo and names

--- frontend/src/components/sliding.jsx
+++ frontend/src/components/sliding.jsx
@@ -3,11 +3,11 @@ import axios from 'axios';
 import Slider from 'react-slick';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
 
 const Sliding = () => {
   const [images, setImages] = useState([]);
   const [error, setError] = useState(null);
+  const [selectedImage, setSelectedImage] = useState(null);
 
   useEffect(() => {
     const fetchImages = async () => {
@@ -20,6 +20,12 @@ const Sliding = () => {
     };
     fetchImages();
   }, []);
+
+  useEffect(() => {
+    if (images.length > 0) {
+      setSelectedImage(images[0]);
+    }
+  }, [images]);
 
   if (error) {
     return <div>Error: {error}</div>;
@@ -35,18 +41,49 @@ const Sliding = () => {
     nextArrow: null,
   };
 
+  const handleImageClick = (imagePair) => {
+    setSelectedImage(imagePair);
+  };
+
   return (
-    <div className='flex justify-center'>
-      <div className='w-11/12'>
-        <Slider {...settings}>
-          {images.map((imagePair, index) => (
-            <div key={index}>
-              <div className=''>
-                <img src={`https://image.tmdb.org/t/p/w500${imagePair[0]}`} alt={`Image ${index}`} className='px-2' />
+    <div className="">
+      {selectedImage && (
+      <div className="flex justify-center relative md:pb-6">
+        <div className="w-11/12">
+          <img
+            src={`https://image.tmdb.org/t/p/w500${selectedImage[1]}`}
+            alt="Selected Image"
+            className="px-2 w-9/12"
+            style={{ filter: "brightness(0.15)" }}
+          />
+          <div className="absolute top-0 left-0 right-0 bg-opacity-50 text-white flex justify-start z-20 p-10 px-32 font-semibold font-rubix text-4xl">
+            <p className="">{selectedImage[2]}</p>
+          </div>
+        </div>
+      </div>
+    )}
+      <div className="flex justify-center relative md:pb-6">
+        <div className="w-11/12">
+          <div className="text-white font-poppins font-semibold text-2xl p-3">
+            <p>Popular</p>
+          </div>
+          <Slider {...settings}>
+            {images.map((imagePair, index) => (
+              <div key={index} className="relative">
+                <div
+                  className="px-1 hover:transition-all hover:duration-300 duration-300 hover:scale-150 hover:px-1"
+                  onClick={() => handleImageClick(imagePair)}
+                >
+                  <img
+                    src={`https://image.tmdb.org/t/p/w500${imagePair[0]}`}
+                    alt={`Image ${index}`}
+                    className="px-2 hover:z-50"
+                  />
+                </div>
               </div>
-            </div>
-          ))}
-        </Slider>
+            ))}
+          </Slider>
+        </div>
       </div>
     </div>
   );
