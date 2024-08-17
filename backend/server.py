d676bfcd8b7607aabd2ea81a3b3d5694f26377d1
Ankit Dey <personal.ankitdey@gmail.com>
2024-08-18 00:42:41 +0530

    finished working site

--- backend/server.py
+++ backend/server.py
@@ -20,27 +20,60 @@ receipts=db["receipts"]
 seats=db["seats"]
 
 app = Flask(__name__)
-cors = CORS(app, origins=["http://localhost:3000", "http://127.0.0.1:5001" , "http://localhost:3000/login"])
+cors = CORS(app, origins=["http://localhost:3000", "http://127.0.0.1:5001" , "http://localhost:3000/login","http://127.0.0.1:5001/book_seats"])
 
 img_path = "https://image.tmdb.org/t/p/w500"
 movie_path_popular = f"https://api.themoviedb.org/3/movie/popular"
 movie_path_genre=f"https://api.themoviedb.org/3/discover/movie"
 api_key = os.getenv("MOVIE_API")
 
+@app.route("/get_finalbill", methods=["POST"])
+def get_finalbill():
+    response=request.get_json()
+    objectid=response['objectId']
+    response=receipts.find_one({'_id':ObjectId(objectid)})
+    seats = [seat for seat in response['seats'] if seat in response['seats']]
+    finalbill={"Bill_id":str(response["_id"]),"Movie":response['movieName'],"Time":response['time'],"Date":response['date'], "Seats": ', '.join(seats)}
+    print(response)
+    return jsonify(finalbill),200
+
 @app.route("/get_booked_seats", methods=["GET"])
 def get_booked_seats():
-    response = availableseats.find_one({"_id": ObjectId('66bf46afe053d1e3839094eb')})
-    response['_id']=str(response['_id'])
+    try:   
+        response = availableseats.find_one({"_id": ObjectId(f'{os.getenv("HALL_ID")}')})
+        response['_id']=str(response['_id'])
+    except:
+        availableseats.create_index("iso_date")
+        iso_date = datetime.datetime.utcnow() + datetime.timedelta(days=1)
+        iso_date = iso_date.replace(hour=0, minute=0, second=0, microsecond=0)
+        availableseats.insert_one({"_id": ObjectId(f'{os.getenv("HALL_ID")}'),"seats":[],"iso_date": iso_date})
+        response = availableseats.find_one({"_id": ObjectId(f'{os.getenv("HALL_ID")}')})
+        response['_id']=str(response['_id'])
     print(response)
     return response,200
+
+@app.route("/book_seats",methods=["POST"])
+def book_seats():
+    response=request.get_json()
+    print("----------")
+    print(response)
+    print("----------")
+    return {"msg":"ok"},200
 
 @app.route("/book_seat",methods=["POST"])
 def book_seat():
     response=request.get_json()
-    response['index']=response['index']+1
+    response['index']=response['index']
     if response['selected']==True:
         fetch_user=receipts.update_one({"_id":ObjectId(response['objectId'])},{"$push":{"seats":str(response['section'])+str(response['index'])}})
-        change_in_hall=availableseats.update_one({"_id": ObjectId('66bf46afe053d1e3839094eb')},{"$push":{"seats":str(response['section'])+str(response['index'])}})
+        print(availableseats.find({}))
+        if availableseats.find({}) == None:
+            availableseats.create_index("iso_date")
+            iso_date = datetime.datetime.utcnow() + datetime.timedelta(days=1)
+            iso_date = iso_date.replace(hour=0, minute=0, second=0, microsecond=0)
+            availableseats.insert_one({"_id": ObjectId('66bf46afe053d1e3839094eb'),"seats":[],"iso_date": iso_date})
+        else:
+            change_in_hall=availableseats.update_one({"_id": ObjectId('66bf46afe053d1e3839094eb')},{"$push":{"seats":str(response['section'])+str(response['index'])}})
     elif response['selected']==False:
         fetch_user=receipts.update_one({"_id":ObjectId(response['objectId'])},{"$pull":{"seats":str(response['section'])+str(response['index'])}})
         change_in_hall=availableseats.update_one({"_id": ObjectId('66bf46afe053d1e3839094eb')},{"$pull":{"seats":str(response['section'])+str(response['index'])}})
@@ -49,14 +82,19 @@ def book_seat():
 
 @app.route("/billing",methods=["POST"])
 def billing():
-    response=request.get_json()
+    response = request.get_json()
     print(response)
-    response["seats"]=[]
-    result=receipts.insert_one(response)
-    objectid=result.inserted_id
-    objectid=str(objectid)
+    response["seats"] = []
+    date_str = response["date"]
+    date_obj = datetime.datetime.strptime(date_str, "%m/%d/%Y")
+    expires_at = date_obj + datetime.timedelta(days=1)
+    expires_at = expires_at.replace(hour=0, minute=0, second=0, microsecond=0)
+    response["expiresAt"] = expires_at
+    result = receipts.insert_one(response)
+    objectid = result.inserted_id
+    objectid = str(objectid)
     print(objectid)
-    return {"objectid":objectid},200
+    return {"objectid": objectid}, 200
 
 @app.route("/searchOneMovie/<string:id>",methods=["POST"])
 def searchOneMovie(id):
